af95a1436428976350d30337cd505782e6be23f3
stevethe1 <104700823+stevethe1@users.noreply.github.com>
2022-05-11 10:58:18 -0400

    Delete product landing page directory

--- product landing page/assets/js/script1.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("nav_link");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
