90f1969f5d0c3d9bc8f48b3e0343e43fede30281
stevethe1 <104700823+stevethe1@users.noreply.github.com>
2022-05-17 18:59:22 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
+
